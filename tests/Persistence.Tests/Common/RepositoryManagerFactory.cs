653798dd304bf43c9763505ddccfbcf9206200bf
MrDave1999 <daveseva2010@hotmail.es>
2025-11-28 09:27:54 -0500

    refactor: use expression-bodied syntax in `RepositoryManagerFactory`

--- tests/Persistence.Tests/Common/RepositoryManagerFactory.cs
+++ tests/Persistence.Tests/Common/RepositoryManagerFactory.cs
@@ -2,14 +2,11 @@
 
 public class RepositoryManagerFactory
 {
-    public static IRepositoryManager Create(DatabaseProvider provider)
+    public static IRepositoryManager Create(DatabaseProvider provider) => provider switch
     {
-        return provider switch
-        {
-            DatabaseProvider.InMemory => new InMemoryRepositoryManager(),
-            DatabaseProvider.MariaDb => new MariaDbRepositoryManager(),
-            DatabaseProvider.Sqlite => new SqliteRepositoryManager(),
-            _ => throw new NotSupportedException($"'{provider}' was not found as a database provider.")
-        };
-    }
+        DatabaseProvider.InMemory => new InMemoryRepositoryManager(),
+        DatabaseProvider.MariaDb => new MariaDbRepositoryManager(),
+        DatabaseProvider.Sqlite => new SqliteRepositoryManager(),
+        _ => throw new NotSupportedException($"'{provider}' was not found as a database provider.")
+    };
 }
