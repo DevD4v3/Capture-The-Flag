457785a69b246bccc4c759e500ee4107b999c75d
MrDave1999 <daveseva2010@hotmail.es>
2026-06-10 14:36:56 -0500

    Upate entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Starting entrypoint..."
+echo "Port=$Port"
+echo "MaxPlayers=$MaxPlayers"
+
 jq "
   .game.lag_compensation_mode = ${LagCompMode}
   | .max_players = ${MaxPlayers}
@@ -9,4 +13,5 @@ jq "
 
 mv config.tmp config.json
 
+echo "Launching omp-server..."
 exec ./omp-server
