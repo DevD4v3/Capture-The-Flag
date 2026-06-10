398181754b02105d31e5ec625e90444fb6d85284
MrDave1999 <daveseva2010@hotmail.es>
2026-06-10 15:12:26 -0500

    chore: add RCON password to generated config.json

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@ echo "Starting entrypoint..."
 echo "LagCompMode=$LagCompMode"
 echo "MaxPlayers=$MaxPlayers"
 echo "Port=$Port"
-echo "ServerPassword=$ServerPassword"
 
 jq "
   .game.lag_compensation_mode = ${LagCompMode}
   | .max_players = ${MaxPlayers}
   | .network.port = ${Port}
   | .password = \"${ServerPassword}\"
+  | .rcon.password = \"${RconPassword}\"
 " config.json > config.tmp
 
 mv config.tmp config.json
