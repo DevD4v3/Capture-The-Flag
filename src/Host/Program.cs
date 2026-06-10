1acd9c38d615f9c9bb52ae613f613f9f216fbd91
MrDave1999 <daveseva2010@hotmail.es>
2026-06-09 19:10:15 -0500

    fix: add temporary entry point while using local SampSharp project references

---
+++ src/Host/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SampSharp
+{
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("SampSharp.SourceGenerator", "1.0.0.0")]
+    public static class Entrypoint
+    {
+        private static readonly global::CTF.Host.Startup _startup = new();
+        private static SampSharp.OpenMp.Core.StartupContext _context;
+        [global::System.Runtime.InteropServices.UnmanagedCallersOnlyAttribute]
+        public static void Initialize(SampSharp.OpenMp.Core.SampSharpInitParams inf)
+        {
+            _context = new SampSharp.OpenMp.Core.StartupContext(inf);
+            _context.InitializeUsing(_startup);
+        }
+
+        public static void Main()
+        {
+            SampSharp.OpenMp.Core.StartupContext.MainInfoProvider();
+        }
+    }
+}
