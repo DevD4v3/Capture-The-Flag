1b78afcb186540695fb647c1a572d37f17c8c22a
MrDave1999 <daveseva2010@hotmail.es>
2026-06-14 21:01:15 -0500

    fix: correct Messages resource manager base name

--- src/Application/Messages.Designer.cs
+++ src/Application/Messages.Designer.cs
@@ -39,7 +39,7 @@ namespace CTF.Application {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CTF.Application.Common.Resources.Messages", typeof(Messages).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CTF.Application.Messages", typeof(Messages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
