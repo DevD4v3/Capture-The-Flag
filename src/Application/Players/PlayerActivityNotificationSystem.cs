0f0eeaeb3aafc8e9803578426ddc9a5c2a01d53a
MrDave1999 <daveseva2010@hotmail.es>
2026-06-16 19:23:54 -0500

    feat: add Discord notifications for player connections

---
+++ src/Application/Players/PlayerActivityNotificationSystem.cs
@@ -0,0 +1,19 @@
+﻿namespace CTF.Application.Players;
+
+public class PlayerActivityNotificationSystem(
+    IDiscordWebhookClient discordWebhookClient) : ISystem
+{
+    [Event]
+    public async Task OnPlayerConnect(Player player)
+    {
+        var message = new DiscordMessage($"🎮 **{player.Name}** connected to the server.");
+        await discordWebhookClient.SendAsync(message);
+    }
+
+    [Event]
+    public async Task OnPlayerDisconnect(Player player, DisconnectReason _)
+    {
+        var message = new DiscordMessage($"👋 **{player.Name}** disconnected from the server.");
+        await discordWebhookClient.SendAsync(message);
+    }
+}
