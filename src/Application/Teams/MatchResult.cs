5fb0bd2413b367365e8ef6fafd2730ba09d8f324
David Román <43916038+DevD4v3@users.noreply.github.com>
2026-06-21 08:14:34 -0500

    refactor: encapsulate match outcome logic in MatchResult (#336)
    * refactor: encapsulate match outcome logic in MatchResult
    
    * chore: checkout submodules in CodeQL workflow
    
    * fix: use matching CodeQL action versions

---
+++ src/Application/Teams/MatchResult.cs
@@ -0,0 +1,25 @@
+﻿namespace CTF.Application.Teams;
+
+public class MatchResult
+{
+    public Team Winner { get; }
+    public string Announcement { get; }
+    public bool IsTie => Winner == Team.None;
+
+    private MatchResult(Team winner, string announcement)
+    {
+        Winner = winner;
+        Announcement = announcement;
+    }
+
+    public static MatchResult Create()
+    {
+        if (Team.Alpha.IsWinner())
+            return new MatchResult(Team.Alpha, Messages.AlphaIsWinner);
+
+        if (Team.Beta.IsWinner())
+            return new MatchResult(Team.Beta, Messages.BetaIsWinner);
+
+        return new MatchResult(Team.None, Messages.TiedTeams);
+    }
+}
---
+++ tests/Application.Tests/Teams/MatchResultTests.cs
@@ -0,0 +1,57 @@
+﻿namespace CTF.Application.Tests.Teams;
+
+public class MatchResultTests
+{
+    [SetUp]
+    public void Init()
+    {
+        Team.Alpha.Reset();
+        Team.Beta.Reset();
+    }
+
+    [Test]
+    public void Create_WhenAlphaTeamWins_ShouldReturnAlphaAsWinner()
+    {
+        // Arrange
+        Team.Alpha.StatsPerRound.AddScore();
+
+        // Act
+        MatchResult result = MatchResult.Create();
+
+        // Assert
+        result.Winner.Should().Be(Team.Alpha);
+        result.IsTie.Should().BeFalse();
+        result.Announcement.Should().Be(Messages.AlphaIsWinner);
+    }
+
+    [Test]
+    public void Create_WhenBetaTeamWins_ShouldReturnBetaAsWinner()
+    {
+        // Arrange
+        Team.Beta.StatsPerRound.AddScore();
+
+        // Act
+        MatchResult result = MatchResult.Create();
+
+        // Assert
+        result.Winner.Should().Be(Team.Beta);
+        result.IsTie.Should().BeFalse();
+        result.Announcement.Should().Be(Messages.BetaIsWinner);
+    }
+
+    [Test]
+    public void Create_WhenNoTeamWins_ShouldReturnTieResult()
+    {
+        // Arrange
+        Team.Alpha.StatsPerRound.AddScore();
+        Team.Beta.StatsPerRound.AddScore();
+
+        // Act
+        MatchResult result = MatchResult.Create();
+
+        // Assert
+        result.Winner.Should().Be(Team.None);
+        result.IsTie.Should().BeTrue();
+        result.Announcement.Should().Be(Messages.TiedTeams);
+    }
+}
