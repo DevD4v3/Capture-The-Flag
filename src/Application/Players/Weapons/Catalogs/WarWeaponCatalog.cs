a622f72d50ce1b82ff13b2fd05e997470b0d2241
MrDave1999 <daveseva2010@hotmail.es>
2026-06-24 15:19:35 -0500

    feat: add heavy and war weapon catalogs
    * Add Heavy Weapons catalog featuring RPGs and miniguns
    * Add War Weapons catalog featuring military and explosive weapons
    * Add weapon definitions for heavy and special-purpose weapons
    * Improve weapon display names shown to players
    * Expand gameplay variety through specialized weapon catalogs

---
+++ src/Application/Players/Weapons/Catalogs/WarWeaponCatalog.cs
@@ -0,0 +1,22 @@
+﻿namespace CTF.Application.Players.Weapons.Catalogs;
+
+public class WarWeaponCatalog : WeaponCatalogBase
+{
+    public override WeaponCatalogType Type => WeaponCatalogType.War;
+
+    protected override void Define(List<IWeapon> weapons)
+    {
+        weapons.AddRange(
+        [
+            WeaponDefinitions.Deagle,
+            WeaponDefinitions.CombatShotgun,
+            WeaponDefinitions.M4,
+            WeaponDefinitions.Sniper,
+            WeaponDefinitions.Grenade,
+            WeaponDefinitions.Molotov,
+            WeaponDefinitions.TearGas,
+            WeaponDefinitions.Flamethrower,
+            WeaponDefinitions.SatchelCharge
+        ]);
+    }
+}
