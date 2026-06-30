24c33acc1431b681763ff29a43ff3e3d02d94b82
MrDave1999 <daveseva2010@hotmail.es>
2026-06-30 07:32:56 -0500

    feat: make ACK limit configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,13 @@ echo "Starting entrypoint..."
 echo "LagCompMode=$LagCompMode"
 echo "MaxPlayers=$MaxPlayers"
 echo "Port=$Port"
+echo "AcksLimit=$AcksLimit"
 
 jq "
   .game.lag_compensation_mode = ${LagCompMode}
   | .max_players = ${MaxPlayers}
   | .network.port = ${Port}
+  | .network.acks_limit = ${AcksLimit}
   | .password = \"${ServerPassword}\"
   | .rcon.password = \"${RconPassword}\"
 " config.json > config.tmp
