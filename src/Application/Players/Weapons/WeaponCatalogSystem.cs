09f239feeccedc26a8cadf68fa276b434685b161
MrDave1999 <daveseva2010@hotmail.es>
2026-07-05 21:15:43 -0500

    refactor: split weapon management into dedicated systems
    Split the weapon management logic into dedicated systems based on
    their responsibilities.
    
    * Introduced WeaponSelectionSystem for weapon selection and weapon packs.
    * Introduced WeaponCatalogSystem for weapon catalog management.
    * Added GunGame validation after dialog completion to prevent dialogs from applying changes after GunGame has been enabled.

---
+++ src/Application/Players/Weapons/WeaponCatalogSystem.cs
@@ -0,0 +1,67 @@
+﻿namespace CTF.Application.Players.Weapons;
+
+public class WeaponCatalogSystem(
+    IEntityManager entityManager,
+    IDialogService dialogService,
+    IGunGameMode gunGameMode,
+    WeaponCatalog weaponCatalog,
+    WeaponCatalogSettings weaponCatalogSettings) : ISystem
+{
+    [PlayerCommand("weaponcatalog")]
+    public async Task ShowCatalogs(Player player)
+    {
+        if (gunGameMode.IsEnabled)
+        {
+            player.SendClientMessage(Color.Red, Messages.WeaponCatalogUnavailable);
+            return;
+        }
+
+        if (player.HasLowerRoleThan(RoleId.Admin))
+            return;
+
+        var dialog = new ListDialog("Weapon Catalogs", "Select", "Close");
+        foreach (WeaponCatalogType type in Enum.GetValues<WeaponCatalogType>())
+            dialog.Add(type.GetDisplayName());
+
+        ListDialogResponse response = await dialogService.ShowAsync(player, dialog);
+        if (response.IsRightButtonOrDisconnected())
+            return;
+
+        if (gunGameMode.IsEnabled)
+        {
+            player.SendClientMessage(Color.Red, Messages.WeaponCatalogUnavailable);
+            return;
+        }
+
+        WeaponCatalogType selectedCatalog = (WeaponCatalogType)response.ItemIndex;
+        if (weaponCatalogSettings.Type == selectedCatalog)
+        {
+            player.SendClientMessage(Color.Red, Messages.WeaponCatalogAlreadyActive);
+            return;
+        }
+
+        weaponCatalogSettings.Change(selectedCatalog);
+        var catalogName = selectedCatalog.GetDisplayName();
+        var message = Smart.Format(Messages.WeaponCatalogChangedTo, new { Name = catalogName });
+
+        foreach (Player currentPlayer in entityManager.GetComponents<Player>())
+        {
+            var weaponSelection = currentPlayer.GetComponent<WeaponSelectionComponent>();
+            WeaponPack selectedWeapons = weaponSelection.SelectedWeapons;
+
+            // Ensure the player's weapon pack only contains weapons
+            // available in the newly selected catalog.
+            selectedWeapons.RemoveAll(weapon =>
+            {
+                bool shouldRemove = !weaponCatalog.Contains(weapon);
+
+                if (shouldRemove)
+                    currentPlayer.RemoveWeapon(weapon.Id);
+
+                return shouldRemove;
+            });
+
+            currentPlayer.SendClientMessage(Color.Yellow, message);
+        }
+    }
+}
