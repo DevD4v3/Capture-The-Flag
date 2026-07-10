69171946c5fe0dc2a7f6bc9d52579abf2e6a89c4
MrDave1999 <daveseva2010@hotmail.es>
2026-07-10 12:41:49 -0500

    refactor: move player role extensions into the Roles submodule

--- src/Application/Players/Accounts/Roles/PlayerRoleExtensions.cs
+++ src/Application/Players/Accounts/Roles/PlayerRoleExtensions.cs
@@ -1,10 +1,11 @@
-﻿namespace CTF.Application.Players.Extensions;
+﻿namespace CTF.Application.Players.Accounts.Roles;
 
 public static class PlayerRoleExtensions
 {
     public static bool HasLowerRoleThan(this Player player, RoleId id)
     {
         PlayerInfo playerInfo = player.GetRequiredInfo();
+
         if (playerInfo.HasLowerRoleThan(id))
         {
             player.SendClientMessage(Color.Red, Messages.NoPermissions);
