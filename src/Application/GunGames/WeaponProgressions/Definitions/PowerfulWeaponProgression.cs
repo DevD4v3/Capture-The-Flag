f14a9a09483ef424a1d0d8a8f9c11f6ddda033d9
MrDave1999 <daveseva2010@hotmail.es>
2026-07-10 20:37:52 -0500

    feat: add powerful weapon progression
    * Added a new GunGame weapon progression featuring the most powerful weapons in GTA, culminating with a grenade as the final challenge.

---
+++ src/Application/GunGames/WeaponProgressions/Definitions/PowerfulWeaponProgression.cs
@@ -0,0 +1,27 @@
+﻿namespace CTF.Application.GunGames.WeaponProgressions.Definitions;
+
+/// <summary>
+/// Defines a GunGame weapon progression featuring the most powerful weapons in GTA.
+/// </summary>
+public class PowerfulWeaponProgression : WeaponProgressionBase
+{
+    public override WeaponProgressionType Type => WeaponProgressionType.Powerful;
+
+    protected override void Define(List<IWeapon> weapons)
+    {
+        weapons.AddRange(
+        [
+            WeaponDefinitions.MP5,
+            WeaponDefinitions.AK47,
+            WeaponDefinitions.M4,
+            WeaponDefinitions.Sawedoff,
+            WeaponDefinitions.CombatShotgun,
+            WeaponDefinitions.Deagle,
+            WeaponDefinitions.Flamethrower,
+            WeaponDefinitions.RocketLauncher,
+            WeaponDefinitions.Heatseeker,
+            WeaponDefinitions.Minigun,
+            WeaponDefinitions.Grenade
+        ]);
+    }
+}
