93403113b631c525f3d278748308ac1f8df4ae77
MrDave1999 <daveseva2010@hotmail.es>
2026-07-14 16:42:19 -0500

    feat: update GunGame wins after completing the weapon progression

--- src/Application/GunGames/Results/PlayerScoredFinalKill.cs
+++ src/Application/GunGames/Results/PlayerScoredFinalKill.cs
@@ -4,16 +4,22 @@
 /// Handles the <see cref="GunGameResult.ScoredFinalKill"/> result.
 /// </summary>
 public class PlayerScoredFinalKill(
-    IWorldService worldService) : IGunGameResultHandler
+    IWorldService worldService,
+    IPlayerRepository playerRepository) : IGunGameResultHandler
 {
     public GunGameResult Result => GunGameResult.ScoredFinalKill;
 
     public void Handle(KillContext context)
     {
+        PlayerInfo killerInfo = context.Killer.GetRequiredInfo();
+        killerInfo.AddGunGameWins();
+        playerRepository.UpdateGunGameWins(killerInfo);
+
         var message = Smart.Format(GunGameMessages.PlayerScoredFinalKill, new
         {
             Killer = context.Killer.Name
         });
+
         worldService.SendClientMessage(Color.Gold, message);
     }
 }
